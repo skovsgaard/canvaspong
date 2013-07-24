778c83b54281838cb723da9a7ed30cd355b42ff2
jschrist <bastian.1343@gmail.com>
2013-07-24 01:58:53 -0400

    Update canvaspong.js

--- canvaspong.js
+++ canvaspong.js
@@ -1,32 +1,36 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var halfWidth = canvas.width / 2;
-var halfHeight = canvas.height / 2;
+var FPS = 30;
+var basePoint = {
+  x: canvas.width / 2 - 10,
+  y: canvas.height / 2 - 10
+}
 
 var ball = {
   width: 20,
   height: 20,
-  x: halfWidth / 2 - (this.width / 2),
-  y: halfHeight / 2 - (this.height / 2),
-  isMoving: false,
-  spawn: function(width, height) {
-    var ballWidth = width;
-    var ballHeight = height;
-    ctx.fillRect(halfWidth-ballWidth,halfHeight-ballHeight,ballWidth,ballWidth);
+  speed: 2,
+  x: basePoint.x,
+  y: basePoint.y,
+  draw: function() {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgb(0,0,0';
+    ctx.fillRect(this.x, this.y, this.width, this.height);
   },
-  move: function(speed) {
-    if (!this.isMoving) {
-      this.isMoving = true;
-      this.x += speed;
+  update: function() {
+    if (this.x <= canvas.width) {
+      this.x += this.speed;
+    } else {
+      this.x = 0;
     }
   }
 }
 
 function main() {
-  ball.spawn(20, 20);
   setInterval(function() {
-    ball.move(10);
-  }, 1);
+    ball.update();
+    ball.draw();
+  }, 1000/FPS);
 }
 
 window.onload = main();
