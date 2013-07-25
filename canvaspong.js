108f2973347f6b878e1304f559f1025585f71b35
skovsgaard <bastian.1343@gmail.com>
2013-07-25 00:43:51 -0400

    Update canvaspong.js

--- canvaspong.js
+++ canvaspong.js
@@ -1,29 +1,63 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var FPS = 30;
+var FPS = 2d0;
 var baseSpeed = 2;
+var upKeyDown = false;
+var downKeyDown = false;
 
-var keyChecks = {
-  up: false,
-  down: false
-}
+function checkKeysDown() {
+  document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 38) {
+      upKeyDown = true;
+      console.log('Upkey pressed');
+    } else if(event.keyCode == 40) {
+      downKeyDown = true;
+      console.log('Downkey pressed');
+    }
+  });
+};
+
+/*function checkKeysUp() {
+  document.addEventListener('keyup', function(event) {
+    if (event.keyCode == 38) {
+      upKeyDown = false;
+      console.log('Upkey released');
+    } else if (event.keyCode == 40) {
+      downKeyDown = false;
+      console.log('Downkey released');
+    }
+  });
+}*/
 
 var player = {
   width: 20,
   height: 100,
   speed: 8,
   x: canvas.width - 40,
-  y: canvas.height - 20,
+  y: canvas.height / 2,
   draw: function() {
     ctx.fillStyle = 'rgb(0,0,0)';
     ctx.fillRect(this.x, this.y, this.width, this.height);
   },
   update: function() {
     if (this.y <= canvas.height) {
-      this.y += this.speed;
-    } else {
-     this.y = 0;
+      if (upKeyDown) {
+        this.y -= this.speed;
+      }
+      if (downKeyDown) {
+        this.y += this.speed;
+      }
     }
+    
+    if (this.y + this.height >= canvas.height) {
+      this.y = canvas.height - this.height;
+    }
+    
+    if (this.y < 0) {
+      this.y = 0;
+    }
+    upKeyDown = false;
+    downKeyDown = false;
   }
 }
 
@@ -39,10 +73,10 @@ var ball = {
     ctx.fillRect(this.x, this.y, this.width, this.height);
   },
   update: function() {
-    if (this.x <= 0 /*&& !this.movingRight*/) {
+    if (this.x <= 0) {
       this.speed = baseSpeed;
       this.x += this.speed;
-    } else if (this.x + this.width > canvas.width /*&& this.movingRight*/) {
+    } else if (this.x + this.width > canvas.width) {
       this.speed *= -1;
       this.x += this.speed;
     } else {
@@ -78,6 +112,7 @@ function draw() {
 
 function main() {
   setInterval(function() {
+    checkKeysDown();
     update();
     draw();
   }, 1000/FPS);
